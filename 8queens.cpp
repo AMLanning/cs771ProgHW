67bcdbb2627308cda2534fbeca3e0ed1a0c3ee57
Avery Lanning <amlanning14@gmail.com>
2021-04-21 19:05:23 -0500

    Set up files, added information header

---
+++ 8queens.cpp
@@ -0,0 +1,14 @@
+/* Avery Lanning
+ * 
+ * CS 771 - Artificial Intelligence
+ * Title: 8 Queens Programs
+ * 
+ * Description: This is my homework solving the 8 queens problem for my Artificial Intelligence course at Wichita State. All code is in one file - per class requirements.
+ * Non-WSU Email: amlanning14@gmail.com
+ */
+
+int main()
+{
+    
+    return 0;
+}
