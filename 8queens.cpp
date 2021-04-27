a9d8ae3ac78d62981542f7965faf87b27e66b601
Avery Lanning <amlanning14@gmail.com>
2021-04-27 12:21:02 -0500

    Restructured and Rewrote

--- 8queens.cpp
+++ 8queens.cpp
@@ -15,7 +15,7 @@
 #include <fstream>
 #include <string>
 
-int getLargestN1Queens(std::vector<int>& queenSet, std::vector<int>& largestN1QueenIndices);
+int getLargestN1Queens(std::vector<int>& queenSet, std::vector<int*>& largestN1Queens);
 int findMostAttackedQueen(std::vector<int>& queenVector);
 void assignBestQueenRow(std::vector<int>& queenVector, int indexInQuestion);
 int getQueenAttackValue(std::vector<int>& queenVector, int indexInQuestion, int valueAttempt = -1);
@@ -23,16 +23,9 @@ int getQueenAttackValue(std::vector<int>& queenVector, int indexInQuestion, int 
 int main()
 {
 //Set up the data structures and variables
-    std::vector<std::vector<int>> allQueenSets; //This vector will start with all of the initial states as their own vector
+    std::vector<std::vector<int>> allQueenSets; //This vector will hold all of the initial states as their own vector
     std::string tmp;
     int numStates;
-    std::vector<bool> resultVec;
-
-//Test code for each function
-    //findMostAttackedQueen Test Code
-    //std::vector<int> tmp = { 0,-1,2,3,4,5,6,7,8 }; //The vector has 9 positions, 0 is unused, to match the homework's index values
-    //allQueenSets.push_back(tmp);//([0,1,2,3,4,5,6,7]);
-    //std::cout << "Most Attacked Queen: " << findMostAttackedQueen(allQueenSets.at(0)) << "\n";
 
 //Loading Initial States from the Input File
     std::ifstream inputFile("infile.txt");
@@ -43,7 +36,7 @@ int main()
 
     for (int i = 0; i < numStates; i++) {
         std::vector<int> singleQueenSet;
-        singleQueenSet.push_back(-1); //This fills position 1 so that the index values can match the homework sheet
+        singleQueenSet.push_back(-1); //This fills index 0 so that the index values can match the homework sheet
 
         for (int j = 0; j < 8; j++) {
             tmp = inputFile.get();
@@ -56,71 +49,52 @@ int main()
         allQueenSets.push_back(singleQueenSet);
     }
 
-//Searching for a Goal State
-    //Loop through vector of queen initial states
-        //Output Initial State
-        //Loop until success/failure
-            //Find queen with most attacks, if tie pick lowest index
-            //Assign Value from function: FindLeastAttackedValue
-                //If it fails, how do I handle failure?
-            //Output current state of array
-            
-            //Check sucess/failure, if looping then neither
-                //Have an if/if-else statement to output success/failure, use double '\n' to make a blank line after each run
-
-    for(int i = 0; i < numStates; i++) {
-        //Get the queenVec containing references to all queens at the current max n1 -- largest number of attacked queens
-        std::vector<int> largestN1Queens;
-        int n1 = getLargestN1Queens(allQueenSets.at(i), largestN1Queens);
-
-        std::cout << "\nresult n1 "<< n1 << "\n";
-
-        //Checking for success
-        if(n1 == 0) {
-            resultVec.push_back(true); //This has been solved
-            break;
-	}
-
-        //Checking for failure
-            //Check each queen with n1 if any have an n2 less than n1 continue, otherwise assign failure
-        for(int j = 0; j < largestN1Queens.size(); j++) {
-            int tmp = largestN1Queens.at(j); //The queen currently under consideration
-
-            //If this particular queen is less than n1 then this is not a failure. It is only a failure when all queens have an attack value over n1
-            if((getQueenAttackValue(allQueenSets.at(i), (allQueenSets.at(i)).at(tmp))) < n1) {
-                break;
-            }else if(j == (largestN1Queens.size() -1)) {
-                resultVec.push_back(false); //This is unsolvable
-            }
-        }
-
-        //After success/failure checks, perform changes
-        assignBestQueenRow(allQueenSets.at(i), largestN1Queens.at(0));
-    }
-
-
-
-
-
-//Outputting the results
+//Setting up the output file
     std::ofstream outfile("outfile.txt");
 
-    for (int i = 0; i < numStates; i++) {
-        for (int j = 1; j < (allQueenSets.at(i)).size(); j++) {
-            outfile << (allQueenSets.at(i)).at(j);
 
-            if(j != (allQueenSets.at(i)).size() - 1) { //Spaces in between, newlines after the states is outputted
-                outfile << " ";
-            }else {
-                outfile << "\n";
-            }
-        }
+//Going through each queenSet and searching for the goal state
+    for(int i = 0; i < numStates; i++) {
+    std::vector<int> queenSet = allQueenSets.at(i);
+
+    //Output the current queen state
+
+
+    //Find the queens with n1 attacks, add their pointers to a vector
+    std::vector<int*> queensWithN1;
+    int n1 = getLargestN1Queens(queenSet, queensWithN1);
+
+    //For each queen with n2, check its attack value
+    if(n1 = 0) {
+        outfile << "Success\n";    
+        break;
+    }
     }
 
+
+//All Done!
     return 0;
 }
 
-int getLargestN1Queens(std::vector<int>& queenSet, std::vector<int>& largestN1QueenIndices)
+void outputCurrentQueenState(std::vector<int> queenSet, std::ofstream& outfile)
+{
+    for(int i = 0; i < 9; i++) {
+        outfile << queenSet.at(i);
+
+        if(i != 8) {
+            outfile << " ";
+        }else {
+            outfile << "\n";
+        }
+    }
+}
+
+bool checkIfFailure(int n1, std::vector<int> queenSet, std::vector<int*> largestN1Queens)
+{
+
+}
+
+int getLargestN1Queens(std::vector<int>& queenSet, std::vector<int*>& largestN1Queens)
 {
     int largestN1 = getQueenAttackValue(queenSet, 1);
 
@@ -136,7 +110,7 @@ int getLargestN1Queens(std::vector<int>& queenSet, std::vector<int>& largestN1Qu
     //Collecting All Queens with this largest n1 value
     for(int i = 1; i < 9; i++) {
         if(getQueenAttackValue(queenSet, i) == largestN1) {
-            largestN1QueenIndices.push_back(i);
+            largestN1Queens.push_back(&(queenSet.at(1))); //Pushes a pointer to this position
         }
     }
 
@@ -203,13 +177,6 @@ int getQueenAttackValue(std::vector<int>& queenVector, int indexInQuestion, int 
     //Now counting the number of attacks that occur
     for (int i = 1; i < 9; i++) {
 
-        //if (queenVector.at(i) == queenVector.at(queenIndex)) { //Same row case
-        //    attacksCount++; //This check has revealed that queen i and the queen at queenIndex are attacking each other by being in the same row.
-        //    continue;
-        //}//else if(abs(i - queenIndex) == abs(queenVector.at(i) - queenVector.at(queenIndex))) { //Diagonal Case
-        //    attacksCount++; //This check has revealed that the two queens are diagonal to each other, and are thus attacking each other.
-        //    continue;
-        //}
     }
 
     return attacksCount;
